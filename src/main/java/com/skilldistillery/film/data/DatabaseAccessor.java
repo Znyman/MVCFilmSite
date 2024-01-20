7eda79a8c3a4c7036ed4de0d599257740c8a62d7
Zach Nyman <zachery.nyman@gmail.com>
2024-01-19 18:01:07 -0700

    DAO implementation

---
+++ src/main/java/com/skilldistillery/film/data/DatabaseAccessor.java
@@ -0,0 +1,24 @@
+package com.skilldistillery.film.data;
+
+import java.util.List;
+
+import com.skilldistillery.film.entities.Actor;
+import com.skilldistillery.film.entities.Film;
+
+public interface DatabaseAccessor {
+	public Film findFilmById(int filmId);
+
+	public Actor findActorById(int actorId);
+
+	public List<Actor> findActorsByFilmId(int filmId);
+
+	List<Film> findFilmsByActorId(int actorId);
+
+	List<Film> findFilmsByKeyword(String keyword);
+	
+	public Film createFilm(Film film);
+	
+	public boolean deleteFilm(Film film);
+	
+	public boolean updateFilm(Film film);
+}
